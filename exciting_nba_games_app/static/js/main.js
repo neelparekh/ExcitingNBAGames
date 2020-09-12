c5fd6713fd58135e0ec13731d4c8d9d51c3ede20
Mehul Shah <mehul@royalcircuits.com>
2020-09-12 16:29:26 -0700

    get rid of wrong static folder

--- exciting_nba_games_app/static/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $("#validateLoading").hide();
-  $("#verifyLoading").hide();
-  $('.toast').toast('show');
-  $('#verifyBtn').click(function(){
-    $("#verifyBtn").hide();
-    $("#verifyLoading").show();
-  });
-});
