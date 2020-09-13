1d37a049f609b0927a15c0cfe740e5a04510f070
Mehul Shah <mehul@royalcircuits.com>
2020-09-13 12:34:12 -0700

    try to show logs and remove debug mode

--- app.py
+++ app.py
@@ -239,6 +239,7 @@ def update_users():
             raise
     else:
         print("\tNo games are currently exciting. :(")
+        sys.stdout.flush()
 
 
 
@@ -247,4 +248,4 @@ if __name__ == "__main__":
     scheduler.add_job(update_users, CronTrigger.from_crontab('* 11-23 * * *'), timezone=pytz.timezone('US/Pacific'))
     # scheduler.add_job(refresh_games_db, 'interval', days=1, start_date='2020-09-10 00:00:00')
     scheduler.start()
-    app.run(debug=True)
+    app.run()
