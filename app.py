135425452f45bae4821b4d8f46bbaac52b90c60c
Mehul Shah <mehul@royalcircuits.com>
2020-09-14 22:08:42 -0700

    24hr cronjob

--- app.py
+++ app.py
@@ -300,7 +300,7 @@ def update_users():
 
 if __name__ == "__main__":
     scheduler = BackgroundScheduler(timezone=pytz.timezone('US/Pacific'))
-    scheduler.add_job(update_users, CronTrigger.from_crontab('* 11-23 * * *'))
+    scheduler.add_job(update_users, CronTrigger.from_crontab('* 0 - 24 * * *'), timezone=pytz.timezone('US/Pacific'))
     # scheduler.add_job(refresh_games_db, 'interval', days=1, start_date='2020-09-10 00:00:00')
     scheduler.start()
     app.run()
